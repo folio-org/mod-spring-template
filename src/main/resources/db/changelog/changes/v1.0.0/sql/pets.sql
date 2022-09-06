55c352b346bec199110a5e0af3ace168807fb398
Nirmal Shah <nirmal_shah@epam.com>
2022-09-06 09:15:27 +0530

    FOLSPRINGB-60 : correction done as per suggestion

--- src/main/resources/db/changelog/changes/v1.0.0/sql/pets.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE pets
-(
-    id                     integer      NOT NULL PRIMARY KEY,
-    name                   VARCHAR(255) NOT NULL UNIQUE,
-    tag                    VARCHAR(255) NOT NULL
-);
